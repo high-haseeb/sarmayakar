651ad38f4aa8423ab554ef2f864a3652b8e42875
abdullah <khujalifamily@gmail.com>
2024-07-24 21:55:21 +0500

    projects page

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Projects from '../components/projects/Projects'
+import Projects from '@/components/projects/Projects'
 
 function page() {
   return (
-    <div className='w-screen h-auto'>
+    <div className='w-screen h-auto '>
         <Projects/>
     </div>
   )
---
+++ components/ZoomParallax/Perallex.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import "../projects/style.css"
+import Image from 'next/image'
+
+function ContactCard() {
+  return (
+    <div class="card">
+  <div class="top-section relative">
+    <video src='/videos/bg.mp4' muted loop autoPlay className='w-full h-full object-cover z-0 absolute rounded-xl'/>
+    <div class="border"></div>
+    <div class="icons">
+      <div class="logo">
+        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 94 94" class="svg">
+          <path fill="white" d="M38.0481 4.82927C38.0481 2.16214 40.018 0 42.4481 0H51.2391C53.6692 0 55.6391 2.16214 55.6391 4.82927V40.1401C55.6391 48.8912 53.2343 55.6657 48.4248 60.4636C43.6153 65.2277 36.7304 67.6098 27.7701 67.6098C18.8099 67.6098 11.925 65.2953 7.11548 60.6663C2.37183 56.0036 3.8147e-06 49.2967 3.8147e-06 40.5456V4.82927C3.8147e-06 2.16213 1.96995 0 4.4 0H13.2405C15.6705 0 17.6405 2.16214 17.6405 4.82927V39.1265C17.6405 43.7892 18.4805 47.2018 20.1605 49.3642C21.8735 51.5267 24.4759 52.6079 27.9678 52.6079C31.4596 52.6079 34.0127 51.5436 35.6268 49.4149C37.241 47.2863 38.0481 43.8399 38.0481 39.0758V4.82927Z"></path>
+          <path fill="white" d="M86.9 61.8682C86.9 64.5353 84.9301 66.6975 82.5 66.6975H73.6595C71.2295 66.6975 69.2595 64.5353 69.2595 61.8682V4.82927C69.2595 2.16214 71.2295 0 73.6595 0H82.5C84.9301 0 86.9 2.16214 86.9 4.82927V61.8682Z"></path>
+          <path fill="white" d="M2.86102e-06 83.2195C2.86102e-06 80.5524 1.96995 78.3902 4.4 78.3902H83.6C86.0301 78.3902 88 80.5524 88 83.2195V89.1707C88 91.8379 86.0301 94 83.6 94H4.4C1.96995 94 0 91.8379 0 89.1707L2.86102e-06 83.2195Z"></path>
+        </svg> */}
+
+        <Image src={'/logos/logo.png'} className='object-cover w-6 h-6 translate-y-[-4px]' width={720} height={1080} />
+      </div>
+
+      <div class="social-media">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" class="svg">
+          <path d="M 9.9980469 3 C 6.1390469 3 3 6.1419531 3 10.001953 L 3 20.001953 C 3 23.860953 6.1419531 27 10.001953 27 L 20.001953 27 C 23.860953 27 27 23.858047 27 19.998047 L 27 9.9980469 C 27 6.1390469 23.858047 3 19.998047 3 L 9.9980469 3 z M 22 7 C 22.552 7 23 7.448 23 8 C 23 8.552 22.552 9 22 9 C 21.448 9 21 8.552 21 8 C 21 7.448 21.448 7 22 7 z M 15 9 C 18.309 9 21 11.691 21 15 C 21 18.309 18.309 21 15 21 C 11.691 21 9 18.309 9 15 C 9 11.691 11.691 9 15 9 z M 15 11 A 4 4 0 0 0 11 15 A 4 4 0 0 0 15 19 A 4 4 0 0 0 19 15 A 4 4 0 0 0 15 11 z"></path>
+        </svg>
+        <svg class="svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
+        </svg>
+        <img src="/logos/whatsapp-128-svgrepo-com.svg" alt="whatsapp" className='h-full fill-[#DCB95F]' />
+      </div>
+    </div>
+  </div>
+  <div class="bottom-section flex flex-col gap-8">
+    <span class="title">Book A Meeting</span>
+    {/* <div class="row row1">
+      <div class="item">
+        <span class="big-text">2626</span>
+        <span class="regular-text">UI elements</span>
+      </div>
+      <div class="item">
+        <span class="big-text">100%</span>
+        <span class="regular-text">Free for use</span>
+      </div>
+      <div class="item">
+        <span class="big-text">38,631</span>
+        <span class="regular-text">Contributers</span>
+      </div>
+    </div> */}
+
+    <div className='text-white w-[90%] text-center'>Welcome to Sermayakar – Your Gateway to Exceptional Real Estate and Investment Management.</div>
+  </div>
+</div>
+  )
+}
+
+export default ContactCard
